974981be9cb7b9e0278bab934e89819f78184e73
Saud Alsaif <sis.7.7.93@gmail.com>
2017-09-28 01:34:47 -0700

    Fixed pedestrian dynamics

--- src/2d/behaviors/Pedestrian_definitions/CrosswalkDriver.jl
+++ src/2d/behaviors/Pedestrian_definitions/CrosswalkDriver.jl
@@ -52,13 +52,13 @@ function Base.rand(model::CrosswalkDriver)
     if any(isnan, model.σ) || any(x -> x<0,model.σ)
         model.a
     else
-        temp = rand(MvNormal([0.0, 0.0], [model.σ[1] 0.0;0.0 model.σ[2]]))
+        temp = rand(MvNormal([model.a.a_lat, model.a.a_lon], [model.σ[1] 0.0;0.0 model.σ[2]]))
         LaneSpecificAccelLatLon(temp[1],temp[2])
     end
 end
-Distributions.pdf(model::CrosswalkDriver, a::LaneSpecificAccelLatLon) = pdf(MvNormal([0.0,0.0],
+Distributions.pdf(model::CrosswalkDriver, a::LaneSpecificAccelLatLon) = pdf(MvNormal([model.a.a_lat,model.a.a_lon],
                                                                                       [model.σ[1] 0.0;0.0 model.σ[2]]),
                                                                                         [a.a_lat, a.a_lon])
-Distributions.sqmahal(model::CrosswalkDriver, a::LaneSpecificAccelLatLon) = sqmahal(MvNormal([0.0,0.0],
+Distributions.sqmahal(model::CrosswalkDriver, a::LaneSpecificAccelLatLon) = sqmahal(MvNormal([model.a.a_lat,model.a.a_lon],
                                                                                       [model.σ[1] 0.0;0.0 model.σ[2]]),
                                                                                         [a.a_lat, a.a_lon])
